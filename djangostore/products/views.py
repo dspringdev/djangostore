f66760519d1f4d0e86c848cb000f3ca0633d337f
dave <dave@daves-MacBook-Pro.local>
2019-04-01 17:05:44 -0400

    Create Products Model

---
+++ djangostore/products/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# /products -> index
+def index(request):
+    return HttpResponse('Hello World')
+
+
+def new(request):
+    return HttpResponse('New Products')
