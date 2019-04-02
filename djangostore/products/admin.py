ce02791efd42f4ee7b2bcd18d3264f66ae30e6a6
dave <dave@daves-MacBook-Pro.local>
2019-04-02 12:07:03 -0400

    CreateProductAdmin

--- djangostore/products/admin.py
+++ djangostore/products/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Product, Offer
 
 
-admin.site.register(Product)
+# ModelAdmin, managing admin model
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Offer)
--- djangostore/products/models.py
+++ djangostore/products/models.py
@@ -7,11 +7,13 @@ class Product(models.Model):
     stock = models.IntegerField()
     image_url = models.CharField(max_length = 2083) # Max length for url
 
+    def __str__(self):
+        return "Product: {}".format(self.name)
 
 class Offer(models.Model):
     code = models.CharField(max_length = 10, blank = False, null = False)
     description = models.CharField(max_length = 255)
     discount = models.FloatField(blank = False, null = False)
 
-    def __str(self):
+    def __str__(self):
         return "Offer: {}".format(self.description)
